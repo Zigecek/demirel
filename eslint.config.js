851dcd937842326f0365915c50d1e33f32a9f25c
Jan Kozohorský <honza007cz@hotmail.com>
2025-01-03 02:05:40 +0100

    4

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,12 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules"] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
     },
     rules: {
